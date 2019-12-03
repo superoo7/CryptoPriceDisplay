bceda709556ba00ebe3c1572764df1b9c18cd675
Lai Weng Han <jlwhoo7@gmail.com>
2019-12-03 15:14:02 +0800

    add in ETH

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import requests
+import time
 from luma.core.interface.serial import spi, noop
 from luma.led_matrix.device import max7219
 from luma.core.legacy import show_message
 from luma.core.legacy.font import CP437_FONT, proportional
-import time
 
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=4, block_orientation=90,
@@ -12,18 +12,24 @@ device = max7219(serial, cascaded=4, block_orientation=90,
 
 def show(msg):
     show_message(device, msg, fill='white',
-                 font=proportional(CP437_FONT), scroll_delay=0.1)
-    time.sleep(15)
+                 font=proportional(CP437_FONT), scroll_delay=0.2)
+    time.sleep(2)
+
+
+def showDebug(msg):
+    print(msg)
+    time.sleep(2)
 
 
 while True:
     response = requests.get(
-        'https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=bitcoin')
+        'https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=bitcoin,ethereum')
     data = response.json()
-    coinPrice = data['bitcoin']['usd']
-    msg = 'Bitcoin: $%s' % coinPrice
-    print(msg)
-    show(msg)
-    show(msg)
-    show(msg)
-    show(msg)
+    print(data)
+    bitcoinPrice = data['bitcoin']['usd']
+    ethereumPrice = data['ethereum']['usd']
+    btcMsg = 'BTC: $%s' % bitcoinPrice
+    ethMsg = 'ETH: $%s' % ethereumPrice
+    for _ in range(20):
+        show(btcMsg)
+        show(ethMsg)
